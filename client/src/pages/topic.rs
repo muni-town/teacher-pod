b1f17fa96c29be043bf021cd7a8c32041a41fcd2
YuKun Liu <41265098+mrxiaozhuox@users.noreply.github.com>
2022-06-04 12:53:34 +0800

    feat: commit code

--- client/src/pages/topic.rs
+++ client/src/pages/topic.rs
@@ -8,21 +8,17 @@ pub fn Topic(cx: Scope) -> Element {
             class: "container mx-auto",
             Card {
                 div {
-                    class: "flex justify-center",
+                    class: "grid grid-rows-3 grid-flow-col gap-4",
                     div {
-                        class: "w-80 h-44",
+                        class: "w-full h-80",
                         img {
-                            class: "rounded-xl brightness-75",
+                            class: "rounded-xl brightness-50 w-full h-full",
                             src: "https://picsum.photos/seed/2/2000/1000"
                         }
-                    }
-
-                }
-                div {
-                    class: "flex justify-center",
-                    p {
-                        class: "font-semibold text-2xl",
-                        "Technology"
+                        p {
+                            class: "text-5xl -translate-y-40 text-white font-bold flex justify-center",
+                            "Technology"
+                        }
                     }
                 }
             }
