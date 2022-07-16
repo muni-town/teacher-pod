b80775f7299db8a1dea6650f41c0dc1758fcda27
YuKun Liu <41265098+mrxiaozhuox@users.noreply.github.com>
2022-07-16 20:37:27 +0800

    feat: commit code

--- client/src/components/list.rs
+++ client/src/components/list.rs
@@ -1,9 +1,6 @@
 use dioxus::prelude::*;
 use tp_models::{account::Account, data::SearchInfo};
 
-use super::card::Card;
-
-
 #[inline_props]
 pub fn SimpleUserList(cx: Scope, data: Vec<Account>) -> Element {
     cx.render(rsx! {
@@ -49,22 +46,54 @@ pub fn SearchResultList(cx: Scope, data: SearchInfo) -> Element {
     let list_display = data.results.iter().map(|v| {
         let title = v.title_original.clone();
         rsx! {
-            a {
-                href: "https://google.com",
-                class: "bg-white dark:bg-gray-800 shadow overflow-hidden sm:rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 text-balck dark:text-white",
                 div {
-                    class: "px-4 py-5 sm:px-6",
-                    h3 {
-                        class: "text-lg font-semibold",
-                        "{title}"
+                    class: "rounded-xl border p-5 shadow-md w-9/12 bg-white",
+                    div {
+                        class: "flex w-full items-center justify-between border-b pb-3",
+                        div {
+                            class: "flex items-center space-x-3",
+                            div {
+                                class: "h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]",
+                                
+                            }
+                            div {
+                                class: "text-lg font-bold text-slate-700",
+                                "Joe Smith"
+                            }
+                        }
+                        div {
+                            class: "flex items-center space-x-8",
+                            button {
+                                class: "rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold",
+                                "Category"
+                            }
+                            div {
+                                class: "text-xs text-neutral-500",
+                                "2 hours ago"
+                            }
+                        }
                     }
-                    img {
-                        class: "w-10 h-108",
-                        src: "{v.image}"
+                    div {
+                        class: "mt-4 mb-6",
+                        div {
+                            class: "mb-3 text-xl font-bold",
+                            "Nulla sed leo tempus, feugiat velit vel, rhoncus neque?"
+                        }
+                        div {
+                            class: "text-sm text-neutral-600",
+                            "Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec. Curabitur eget feugiat massa"
+                        }
+                    }
+                    div {
+                        div {
+                            class: "flex items-center justify-between text-slate-500",
+                            div {
+                                class: "flex space-x-4 md:space-x-8",
+                            }
+                        }
                     }
                 }
             }
-        }
     });
 
     cx.render(rsx! {
