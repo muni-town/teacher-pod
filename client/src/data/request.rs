3c7d62c8911ceffadbdf58a0d81a699fadd042fc
mrxiaozhuox <mrxzx.info@gmail.com>
2022-07-21 18:02:03 +0800

    feat: commit code

--- client/src/data/request.rs
+++ client/src/data/request.rs
@@ -17,14 +17,14 @@ pub async fn root_path() -> &'static str {
     if let Ok(resp) = resp {
         let res = resp.text().await;
         if res.is_err() {
-            return "http://146.190.40.243:3000/";
+            return "https://teacherpod-server.mrxzx.info/";
         } else {
             let res = res.unwrap();
             if res.is_empty() || resp.status() != 200 {
-                return "http://146.190.40.243:3000/";
+                return "https://teacherpod-server.mrxzx.info/";
             }
             return Box::leak(Box::new(res));
         }
     }
-    "http://146.190.40.243:3000/"
+    "https://teacherpod-server.mrxzx.info/"
 }
